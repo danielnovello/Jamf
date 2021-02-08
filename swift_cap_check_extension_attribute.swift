0365d0c5a756c6bbeade8ffb3298a97c46d098b5
Daniel Novello <djquazzi@gmail.com>
2021-02-08 16:48:07 +0200

    Extension attribute for Jamf

---
+++ swift_cap_check_extension_attribute.swift
@@ -0,0 +1,26 @@
+#!/usr/bin/swift
+//Extension attribute for Jamf
+import Foundation
+
+//ByteCountFormatter
+let formatter = ByteCountFormatter()
+formatter.allowedUnits = .useAll
+formatter.countStyle = .file
+formatter.includesUnit = true
+formatter.isAdaptive = true
+
+//Calculate
+let fileURL = URL(fileURLWithPath:"/")
+do {
+    let values = try fileURL.resourceValues(forKeys: [.volumeAvailableCapacityForImportantUsageKey])
+    if let capacity = values.volumeAvailableCapacityForImportantUsage {
+        //Pretty
+        print("<result>\(formatter.string(fromByteCount: (capacity)))</result>")
+        //Bytes
+        //print(capacity)
+    } else {
+        print("Capacity is unavailable")
+    }
+} catch {
+    print("Error retrieving capacity: \(error.localizedDescription)")
+}
