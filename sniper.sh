3964d1c3b5cbe21b39e6081cda4eb6ad1e307b69
Daniel Novello <djquazzi@gmail.com>
2021-04-22 13:27:05 +0200

    Update sniper.sh

--- sniper.sh
+++ sniper.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 ################################################################
 #### Snipe URL
-snipe_url="http://localhost:8888/api/v1"
+snipe_url="YOURURL/api/v1"
 #### Snipe API key
-snipe_api_key="eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiIxIiwianRpIjoiYzM0MTY0YzRhZWJiMThhZTgwMjQxNmNjMjUyZGVjMjdhNDYxMjJjZWEwZWYwYjViZjVjMTg0ZWY4ZGYxNTA5NWVhMTkxZmRjMzgyY2Y1ODUiLCJpYXQiOjE2MTkwOTA2MjcsIm5iZiI6MTYxOTA5MDYyNywiZXhwIjoyODgxMzk0NjI3LCJzdWIiOiIxIiwic2NvcGVzIjpbXX0.Uklw8Ma-ROeJkAhw9vOijnQDjwQMN7npgzdPPrsTrhyWZlUm5iw85ucss2xB-1Emq7bAepHR1dJdnDF4zp95JndWrUNUFXzAIh0A1jFSaEvYNYSX4pLP7SapZ2GQmV3IUOl9WCFwlKdpzQ-inZEt9HYsJhE6RHzFhCYP9SeW3u7SFGyPuo9NwxiVySxTZuL7bn5m5eYnR_z7qoIoGc1bFcVodeISy-wNR6VAtPY7XzVV6En5eSHa1V0BZ9q3q_vu2fHH3osPFW5kzyD200S1X2vQw-3PG62xeOJhj-Tc4zrOm6oBcidMzibXYb9FAoqGn-pcSN11WE-mmiU5rSHqmRgsxhjV0W8tJ7u6AJ5CGboOMreEaiTYtDjL93igCseR692B8FXSOGPBBTaKHUDQKe5HFytdwbvcPjJMunmUISCwJgNPcdW1aXiQhhTbWbAvUeZDSlzAj8yFnjTgRjOp51FfUUCLtEq0LKRsa4-ULxiTYCwgTBdJ6psNKAk5m0KueiMiSxly4PajJotyU7WDCUnGvImp-rW_HVry8nK8_Ct4WirlzkaBx_n7giFqob4EZR7Ru8sDWQQWMY1sQKvZfUVRB7POWfAoP8-4v9eV1GjhQSJxDULS_Q2366udGmAEa_8mt6TAyc8jr0RAQOOFvcTq4IJ1bSGEaF_836wBUdM"
+snipe_api_key="YOURAPIKEY"
 ################################################################
 
 ### Mac Variables
