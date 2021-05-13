eb6a7291bd52d492c94442e47dea727fe9eedd47
Daniel Novello <djquazzi@gmail.com>
2021-05-13 16:48:03 +0200

    Update
    Updated to alert every 25%

--- macOS_download_SU.sh
+++ macOS_download_SU.sh
@@ -3,41 +3,59 @@
 
 # Log file
 log_file="/private/tmp/SU_download.log"
+# Check file
+check_file="/private/tmp/SU_download_check.txt"
+
+# Clear out old log files
+if [[ -f "$log_file" && "$check_file" ]]
+then 
+  rm "$log_file" && rm "$check_file"
+fi  
+
+# Creat new files
+touch "$check_file"
 
 # Run the download and send it to the background
 echo "Starting macOS download"
 loggedInUser=$(scutil <<< "show State:/Users/ConsoleUser" | awk '/Name :/ { print $3 }')
 /usr/bin/nohup sudo -u "$loggedInUser" /usr/sbin/softwareupdate --fetch-full-installer >> "$log_file" &
+echo "macOS Installer download Started"|xargs -I {} osascript -e 'display notification "{}" with title "macOS Download"'
 
 # Wait for log file to populate (Give it more time to be safe)
-sleep 120
+sleep 15
 
 # Get the current percentage downloaded
 current=$(tail -1 "$log_file" | awk -F':' 'END{ print int($NF) }')
 
-if [[ -f "$log_file" ]]
+if [[ -f "$log_file" && "$check_file" ]]
 then
     while [[ "$current" -ne 0 && "$current" -le 100 ]]
       do
         current=$(tail -1 "$log_file" | awk -F':' 'END{ print int($NF) }')
+        check_current=$(tail -1 "$check_file" | awk 'END{ print }')
         if [[ "$current" -eq 100 ]]
         then
           echo "macOS Installer $current% downloaded"
           # Display Alert, use whatever you want. (Jamfhelper, Terminal-notifier, etc)
           echo "macOS Installer $current% downloaded"|xargs -I {} osascript -e 'display notification "{}" with title "macOS Download"'
           echo "Removing log file: $log_file"
-          rm "$log_file" && exit 1
+          rm "$log_file" && rm "$check_file" exit 1
         elif [[ "$current" -eq 0 ]]
         then 
           echo "Failed download for some reason. Check the log file at $log_file. Exiting"
           exit 1
         else
           echo "Still Downloading macOS Intaller, its on $current%"
+          if [[ "$current" -eq 38 || "$current" -eq 40 ]] && [[ "$check_current" != "$current" ]]
+          then 
+            echo "macOS Installer $current% downloaded"|xargs -I {} osascript -e 'display notification "{}" with title "macOS Download"'
+            echo "$current" >> "$check_file"
+          fi
         fi
          # Extend if you feel it runs too often.
          sleep 10
     done
 else
-   echo "$log_file does not exist, exiting."
+   echo "$log_file and / or $check_file does not exist, exiting."
    exit 0
 fi
